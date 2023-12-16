dcdcec4435db3c5327368f3968efcd7ac0f8a0d3
Brayden Suzuki <100186248+braydens02@users.noreply.github.com>
2023-12-15 19:32:06 -1000

    Create spotascholar.m

---
+++ projects/spotascholar.m
@@ -0,0 +1,18 @@
+---
+layout: project
+type: project
+image: img/animalsquare.png
+title: "Spot a Scholar"
+date: 2022
+published: true
+labels:
+  - Software Engineering
+  - Web Development
+summary: "A web application designed to assist students in finding study groups and tutors, specifically for ICS classes at UH Manoa."
+---
+
+Spot a Scholar is a web application that me and my team at CBAGL developed. The purpose of Spot a Scholar is to provide students an easy way to look for tutors or other like-minded students willing to study together. The web application works by first requiring the user to sign up for an account, asking the user to provide important information such as their name, the classes they are interested in, a username, and a password to log in to their account. In the landing page, there is a navigation bar that takes the user to the various features of the web app. The list classes feature lists all of the available ICS classes that study groups can be formed for. The create meeting page allows a user to set a certain time, date, and location for a study meeting for a certain class. In the list meetings page, all available meetings are listed for users to easily access and join. There is also a leaderboard page that shows the top users of the website, which works by allocating points to users whenever they sign up for or create a meeting. My contributions to this project included creating the list meeting and list classes page, as well as working with some of the server side collections that managed the meetings. I also worked on having the list meeting page have the join meeting feature for each listed meeting. During this project, I learned a great deal about web application development, particularly with Meteor and React. I also learned how working on a team is when it comes to software engineering, as managing the entire project, the individual tasks, and how those tasks were evenly allocated amongst team members was a lot of work. 
+
+<img src="../img/Screenshot 2023-09-01 132812.png" width="600" height="400">
+
+The Github link to our organization page: <a href="https://spot-a-scholar.github.io/"><i class="large github icon "></i>https://spot-a-scholar.github.io/</a>
